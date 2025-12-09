28dd7417bb86c385c5e3efe16f00407b96c40860
AA <180617768+jhshin23@users.noreply.github.com>
2025-12-09 22:17:00 +0900

    변수명 수정, 주석 보완

--- circuit.py
+++ circuit.py
@@ -5,19 +5,20 @@ import busio
 import Adafruit_MCP3008
 
 # LED를 켜고 끄는 함수
-def controlLED(led, on_off): # led 번호의 핀에 on_off(0/1) 값 출력하는 함수
+def controlLED(led, on_off): # led 번호의 핀에 on_off(0: off/1: on) 값 출력하는 함수
         if (on_off):
                 print("on") 
         else:
                 print("off")
         GPIO.output(led, on_off)
 	
-def increaseLED():
+#LED가 점점 밝아지는 함수
+def increaseLED(): # 1초동안 듀티 사이클을 0~99로 1씩 증가
         pwm_green = GPIO.PWM(led_green, 100)
         pwm_yellow = GPIO.PWM(led_yellow, 100)
         pwm_green.start(0) 
         pwm_yellow.start(0) 
-        for value in range(0, 100):
+        for value in range(0, 100): # 1초동안 루프
                 if(buttonFlag):
                         break
                 pwm_green.ChangeDutyCycle(value) 
@@ -26,13 +27,13 @@ def increaseLED():
         pwm_green.stop()
         pwm_yellow.stop() 
         
-
-def decreaseLED():
+#LED가 점점 어두워지는 함수
+def decreaseLED(): # 1초동안 듀티 사이클을 99~0으로 1씩 감소
         pwm_green = GPIO.PWM(led_green, 100)
         pwm_yellow = GPIO.PWM(led_yellow, 100)
         pwm_green.start(100) 
         pwm_yellow.start(100) 
-        for value in range(99, -1, -1):
+        for value in range(99, -1, -1): #1초동안 루프
                 if(buttonFlag):
                         break
                 pwm_green.ChangeDutyCycle(value) 
@@ -41,7 +42,8 @@ def decreaseLED():
         pwm_green.stop()
         pwm_yellow.stop() 
         
-def celebrateAuthLED():
+# LED를 1초에 5번 깜빡이는 함수
+def celebrateAuthLED(): 
         for i in range(5):
                 controlLED(led_green, 0)
                 controlLED(led_yellow, 0)
@@ -55,7 +57,7 @@ def celebrateAuthLED():
 #스위치 콜백 함수
 def isButton_pressed(channel):
         global buttonFlag
-        buttonFlag = True
+        buttonFlag = True 
  
 # 초음파 센서를 제어하여 물체와의 거리를 측정하여 거리 값 리턴하는 함수
 def measure_distance():
@@ -80,59 +82,61 @@ def measure_distance():
 
 def measure_temp_humi():
         global sensor
-        print(float(sensor.temperature), float(sensor.relative_humidity))
+        # HTU21D 장치로부터 온도, 습도 값 읽어 리턴
         return float(sensor.temperature), float(sensor.relative_humidity)
 
 def auth_temp_humi():
         global buttonFlag
-        gapT = 1
-        gapH = 7
-        hotT = 29
-        humidH = 55
-        result = False
-        base_temp, base_humi = [], []
-        end_time = time.time() + 2
+        THRESHOLD_TEMP = 1 # 온도 변화 기준 
+        THRESHOLD_HUMID = 7 # 습도 변화 기준
+        THRESHOLD_SWITCH_TEMP = 29 # 스위치 인증으로 바꾸는 기준 온도
+        THRESHOLD_SWITCH_HUMID = 55 # 스위치 인증으로 바꾸는 기준 습도
+        result = False # 인증 결과를 저장
+        base_temp, base_humi = [], [] #인증 요청 직후의 온습도 측정값을 저장
+        end_time = time.time() + 2 # 온습도 평균을 구하기 위한 2초 
         while time.time() < end_time:
-            time.sleep(1)
+            time.sleep(1) # 1초 잠자기
             t, h = measure_temp_humi()
             base_temp.append(t)
             base_humi.append(h)
-
+        #이렇게 사용자 몰래 2초동안 온습도 평균을 구해둠
         avrgT = sum(base_temp) / len(base_temp)
         avrgH = sum(base_humi) / len(base_humi)
-
-        if(avrgT > hotT or avrgH  > humidH):
-                deadline = time.time() + 20
-                while time.time() < deadline:
+        # 너무 덥거나 습하면 입김으로 변화폭이 적어지므로 스위치 인증 전환
+        if(avrgT > THRESHOLD_SWITCH_TEMP or avrgH  > THRESHOLD_SWITCH_HUMID):
+                deadline = time.time() + 20 #입김보다 제한시간이 10초 더 길음
+                while time.time() < deadline: #LED가 서서히 점멸하여 스위치 전환을 알림
                     increaseLED()
-                    result = buttonFlag
-                    buttonFlag = False
+                    result = buttonFlag #버튼 눌렸나 확인
+                    buttonFlag = False # 인증 성공 대비 초기화
                     if(result):
-                        celebrateAuthLED()
+                        celebrateAuthLED() #인증성공 축하등
                         break
                     decreaseLED()
-                    result = buttonFlag
-                    buttonFlag = False
+                    result = buttonFlag #버튼 눌렸나 확인
+                    buttonFlag = False # 인증 성공 대비 초기화
                     if(result):
-                        celebrateAuthLED()
+                        celebrateAuthLED() #인증성공 축하등
                         break
-                return result
+                return result #인증결과 내어주기
 
-        controlLED(led_green, 1) #인증 준비 끝, 학생에게 알린다
-        controlLED(led_yellow, 1) #인증 준비 끝, 학생에게 알린다
-        deadline = time.time() + 10
+        controlLED(led_green, 1) #입김 인증 준비 끝, LED 켬 
+        controlLED(led_yellow, 1) #입김 인증 준비 끝, LED 켬 
+        deadline = time.time() + 10 #제한시간 10초
         while time.time() < deadline:
-            t, h = measure_temp_humi()
-            if (t-avrgT)>=gapT and (h-avrgH)>=gapH:
+            t, h = measure_temp_humi() #현재 온습도 측정결과
+            #온도 습도 둘 중 하나만이라도 기준값 이상 변하면 인증성공
+            if (t-avrgT)>=THRESHOLD_TEMP and (h-avrgH)>=THRESHOLD_HUMID: 
                    result = True
-                   celebrateAuthLED()
+                   celebrateAuthLED() # 입김 인증 축하등
                    return result
+            time.sleep(1) # 1초 잠자기
         controlLED(led_green, 0)
         controlLED(led_yellow, 0)
-        return result
+        return result # 인증 결과 내어주기
 
 #조도
-def getLight():
+def getLight(): # MCP3202의 CH0에 연결된 조도 센서로부터 조도값 읽기
        return mcp.read_adc(0)
 
 # 초음파 센서를 다루기 위한 전역 변수 선언 및 초기화
@@ -150,17 +154,18 @@ GPIO.setup(led_green, GPIO.OUT)
 GPIO.setup(led_yellow, GPIO.OUT)
 
 #온도
-sda = 2
-scl = 3
+sda = 2 #GPIO2핀. sda 이름이 붙여진 핀
+scl = 3 #GPIO3핀. scl 이름이 붙여진 핀
 
-i2c = busio.I2C(scl, sda)
-sensor = HTU21D(i2c)
+i2c = busio.I2C(scl, sda) # I2C 버스 통신을 실행하는 객체 생성
+sensor = HTU21D(i2c) # I2C 버스에서 HTU21D 장치를 제어하는 객체 생성
 
 #조도
 mcp = Adafruit_MCP3008.MCP3008(clk=11, cs=8, miso=9, mosi=10)
 
 #스위치
-button = 21
-GPIO.setup(button, GPIO.IN, GPIO.PUD_DOWN)
+button = 21 #GPIO21 
+GPIO.setup(button, GPIO.IN, GPIO.PUD_DOWN) # button 핀을 입력으로 설정
+#스위치를 누르면 10ms의 디바운스 후 isButton_pressed가 호출되도록 설정
 GPIO.add_event_detect(button, GPIO.RISING, isButton_pressed, bouncetime=10)
-buttonFlag = False 
+buttonFlag = False #버튼이 눌러진 상태, True면 눌러짐 
